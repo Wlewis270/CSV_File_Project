2fa9cbfdc102b39fb4c5e82110f3a8b8938a600a
Wlewis270 <85946758+Wlewis270@users.noreply.github.com>
2021-12-09 11:19:26 +0000

    pooog

--- CSV_project/Source.cpp
+++ CSV_project/Source.cpp
@@ -1,1 +1,32 @@
-#include 
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+const int ROWS = 15;
+const int COLS = 15;
+
+void main() {
+	int array2d[ROWS][COLS];
+	ifstream arrayInputFile;
+	string strLine;
+
+	arrayInputFile.open("TSP10.csv", ios::in);
+	
+	string strVal;
+	for (int row = 0; row < ROWS; row++) {
+		arrayInputFile >> strLine;
+		int startPos = 0;
+		int pos = strLine.find(",");
+		for (int col = 0; col < COLS - 1; col++) {
+			strVal = strLine.substr(startPos, pos - startPos);
+			array2d[row][col] = atoi(strVal.c_str());
+			startPos = pos + 1;
+			pos = strLine.find(",", startPos);
+		}
+		strVal = strLine.substr(startPos, strLine.length() - startPos);
+		array2D[row][COLS - 1] = atoi(strVal.c_str());
+	}
+
+}
